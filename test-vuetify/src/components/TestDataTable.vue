c62bcff96784032650e15506dc06362b28835acb
Clément BOISSARD <clement.boissard@gmail.com>
2020-03-04 15:48:30 +0100

    Modif : datable, custom pagination

--- test-vuetify/src/components/TestDataTable.vue
+++ test-vuetify/src/components/TestDataTable.vue
@@ -16,13 +16,12 @@
                 :headers="colonnes"
                 :items="lignes"
                 item-key="nom"
-                :items-per-page="10"
                 show-select
                 multi-sort
                 :sort-by="['categorie', 'valeur']"
                 :sort-desc="[false, true]"
                 show-group-by
-                :group-by="groupes"
+                group-by="categorie"
                 :group-desc="groupDirection"
                 :search="search"
                 single-expand
@@ -30,10 +29,14 @@
                 show-expand
                 :loading="loading"
                 loading-text="Chargement des données..."
+                hide-default-footer
+                :page.sync="page"
+                :items-per-page="itemsPerPage"
+                @page-count="pageCount = $event"
             >
                 <!-- group-by="categorie" group-desc="true"   -->
-                <template v-slot:top v-if="groupes !== '' && groupes.length !== 0">
-                    <v-switch v-model="groupDirection" label="Group Decroissant"  class="pa-3"></v-switch>
+                <template v-slot:top>
+                    <v-switch v-model="groupDirection" label="Group Decroissant" class="pa-3"></v-switch>
                 </template>
                 <template v-slot:item.sup10="{ item }">
                     <!--v-simple-checkbox :value="item.valeur > 10" disabled></v-simple-checkbox-->
@@ -46,6 +49,25 @@
                     <v-chip :color="getColor(item.valeur)" dark>{{ item.valeur }}</v-chip>
                 </template>
             </v-data-table>
+            <v-row align="center" justify="center" class="pt-2">
+                <v-col lg="1">
+                    <v-text-field
+                        :value="itemsPerPage"
+                        label="Nombre par page"
+                        type="number"
+                        solo
+                        min="-1"
+                        max="100"
+                        class="mt-6"
+                        dense
+                        @input="itemsPerPage = parseInt($event, 10)"
+                    ></v-text-field>
+                </v-col>
+                <v-col lg="6">
+                    <v-pagination v-model="page" :length="pageCount"></v-pagination>
+                </v-col>
+                <v-col lg="1"></v-col>
+            </v-row>
         </v-card>
     </v-container>
 </template>
@@ -57,6 +79,9 @@ export default {
     name: "TestDataTable",
     data() {
         return {
+            page: 1,
+            pageCount: 0,
+            itemsPerPage: 3,
             search: "",
             loading: true,
             lignes: [],
@@ -66,13 +91,15 @@ export default {
         };
     },
     watch: {
-        lignes(nouvelles){
-            this.lignes.forEach(element => {element.sup10 = element.valeur > 10});
+        lignes(nouvelles) {
+            this.lignes.forEach(element => {
+                element.sup10 = element.valeur > 10;
+            });
         },
         groupes: {
             immediate: true,
-            handler(newVal, oldVal){
-                console.log(newVal, oldVal, this.groupes)
+            handler(newVal, oldVal) {
+                console.log(newVal, oldVal, this.groupes);
             }
         }
     },
@@ -81,7 +108,7 @@ export default {
         setTimeout(() => {
             this.lignes = this.items;
             this.loading = false;
-        }, 2300); // 2300
+        }, 530); // 2300
     },
     methods: {
         getColor(valeur) {
@@ -89,8 +116,8 @@ export default {
             else if (valeur > 10) return "orange";
             else return "green";
         },
-        test(){
-            console.log(this.groupes)
+        test() {
+            console.log(this.groupes);
         }
     }
 };
