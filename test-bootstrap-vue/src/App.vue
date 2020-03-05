d85f10f0ae708b48d4e9dad0f67c2cd284cc3ec9
Clément BOISSARD <clement.boissard@gmail.com>
2020-03-05 14:29:47 +0100

    Add : base to test bootstrap-vue

---
+++ test-bootstrap-vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+		<TestForm></TestForm>
+		<TestDataTable></TestDataTable>
+	</div>
+</template>
+
+<script>
+import TestDataTable from "./components/TestDataTable";
+import TestForm from "./components/TestForm";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {
+    name: "App",
+    components: {
+		TestDataTable,
+		TestForm
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ test-bootstrap-vue/src/components/TestForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestForm',
+
+}
+</script>
+
+<style scoped>
+
+</style>
