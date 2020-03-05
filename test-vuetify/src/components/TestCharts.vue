a1adced75356be82dbe8aa9c07be24bcdc8c2b10
Clément BOISSARD <clement.boissard@gmail.com>
2020-03-05 22:41:44 +0100

    Add : futur file for data transformation in order to match charts needs

--- test-vuetify/src/components/TestCharts.vue
+++ test-vuetify/src/components/TestCharts.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row>
         <v-col cols="12" md="6" v-for="(chart, index) in charts" :key="index">
-            <v-card class="elevation-6 my-5 pa-5" >
+            <v-card class="elevation-6 my-5 pa-5">
                 <v-card-title>{{index}}. {{ chart.titre }}</v-card-title>
                 <canvas :id="'chart-'+index"></canvas>
             </v-card>
@@ -16,6 +16,7 @@ import Objet from "../model/objet";
 import { Categories } from "../model/categories";
 
 import chart1_data from "../data/chart1";
+import transformObjet from '../data/transformObjets'
 
 export default {
     name: "TestCharts",
@@ -25,27 +26,77 @@ export default {
     data() {
         return {
             charts: [
-                { data: chart1_data, titre: "Test" },
                 {
-                    data: chart1_data,
-                    titre: "Nombre de lune par planete et le poid de la planete"
+                    titre: "Test",
+                    type: "pie",
+                    data: this.objetsToPieByCategorie(this.objets),
+                    options: chart1_data.options
+                },
+                {
+                    titre:
+                        "Nombre de lune par planete et le poid de la planete",
+                    type: "line",
+                    data: chart1_data.data,
+                    options: chart1_data.options
                 }
             ]
         };
     },
+    computed: {
+        /*charts() {
+      return [
+        {
+          titre: "Test",
+          type: "line",
+          data: chart1_data.data,
+          options: chart1_data.options
+        },
+        {
+          titre: "Nombre de lune par planete et le poid de la planete",
+          type: "line",
+          data: chart1_data.data,
+          options: chart1_data.options
+        }
+      ];
+    }*/
+    },
     mounted() {
-        console.debug(this.objets)
-        this.charts.forEach((chart, index) => this.createChart('chart-'+index, chart.data));
+        console.debug(this.objets);
+        this.charts.forEach((chart, index) =>
+            this.createChart(
+                "chart-" + index,
+                chart.type,
+                chart.data,
+                chart.options
+            )
+        );
+    },
+    watch: {
+        /*charts: {
+            immediate: true,
+            handler(newVal, oldVal) {
+                console.debug(this.objets);
+                this.charts.forEach((chart, index) =>
+                    this.createChart(
+                        "chart-" + index,
+                        chart.type,
+                        chart.data,
+                        chart.options
+                    )
+                );
+            }
+        }*/
     },
     methods: {
-        createChart(chartId, chartData) {
+        createChart(chartId, type, data, options) {
             const ctx = document.getElementById(chartId);
             const myChart = new Chart(ctx, {
-                type: chartData.type,
-                data: chartData.data,
-                options: chartData.options
+                type: type,
+                data: data,
+                options: options
             });
-        }
+        },
+        ...transformObjet.methods
     }
 };
 </script>
---
+++ test-vuetify/src/data/transformObjets.js
@@ -0,0 +1,14 @@
+export default {
+    methods: {
+        objetsToPieByCategorie: function(objets){
+            console.log("objetsToPieByCategorie", objets)
+            //categories = []
+            objets.forEach(element => {
+                console.log(element)
+            });
+            return {
+
+            }
+        }
+    }
+}
